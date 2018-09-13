9710d9eb4712cc4a82f33d61d22f6317d07a9ba6
Jansen <jansen.goyena@yahoo.com>
2018-09-13 13:48:11 +0800

    wildcard will compile and exact same class name import will not

--- chapter1/understanding_imports/AmbiguousImport.java
+++ chapter1/understanding_imports/AmbiguousImport.java
@@ -1,8 +1,14 @@
 import java.util.*;
 import java.sql.*;
-
+//import java.util.Date;
+//import java.sql.Date;
 public class AmbiguousImport{
-/* if Date Object is not declared this will still compile!
+/* if Date Object is not declared and imports are wildcard
+ * this will still compile!
+ * 
+ * if Date object is imported exactly as the commented import.
+ * compilation error!
+ *
  * if its declared it will say "***Date is ambiguous"
  */
    Date date;
