eaac2874f3998f317e9f120af02b9bad10ac9bab
Jansen <jansen.goyena@yahoo.com>
2018-09-13 13:00:50 +0800

    int can accomodate 10digits

--- chapter1/HelloArgs.java
+++ chapter1/HelloArgs.java
@@ -14,7 +14,8 @@ public class HelloArgs{
  **/ 
 
 public static void main(String args[]){
-
+	//int have max of 10 digits
+	int x=1_234_567_890;
 	System.out.println("Hello Args[0]: "+args[0]);
 
 	System.out.println("Hello Args[1]: "+args[1]);
