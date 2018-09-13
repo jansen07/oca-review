39ec909c093968ffed6ea350e7075ac82810b40a
Jansen <jansen.goyena@yahoo.com>
2018-09-13 12:47:09 +0800

    added HelloArgs
    comments can be single line or multiline and makesure it has a closing tag

---
+++ chapter1/HelloArgs.java
@@ -0,0 +1,20 @@
+public class HelloArgs{
+/*multiline */
+//oneline comment
+	
+/**
+ * important
+ * Nice
+ * //test
+ *
+ **/
+
+public static void main(String args[]){
+
+	System.out.println("Hello Args[0]: "+args[0]);
+
+	System.out.println("Hello Args[1]: "+args[1]);
+
+}
+
+}
