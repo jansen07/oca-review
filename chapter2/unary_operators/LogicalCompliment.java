5bfa2e77e0a328f76b009775970367241f89de2e
Jansen <jansen.goyena@yahoo.com>
2018-09-14 13:35:01 +0800

    Logical Negation ! symbol

---
+++ chapter2/unary_operators/LogicalCompliment.java
@@ -0,0 +1,14 @@
+public class LogicalCompliment{
+
+
+	public static void main(String[] args){
+	boolean x=false;
+	x = !x;
+	System.out.println(x);
+
+// you can't assign 1 or 0 to boolean
+	boolean y=1;
+// can't convert type int to boolean	
+	}
+
+}
