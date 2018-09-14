cabdb0a7d50f944c5585691348c160e44ac30ee8
Jansen <jansen.goyena@yahoo.com>
2018-09-14 13:39:55 +0800

    LogicalCompliment ! and Negation -

---
+++ chapter2/unary_operators/LogicalComplimentNegation.java
@@ -0,0 +1,19 @@
+public class LogicalComplimentNegation{
+
+
+	public static void main(String[] args){
+	boolean x=false;
+	x = !x;
+	System.out.println(x);
+
+// you can't assign 1 or 0 to boolean
+// can't convert type int to boolean
+
+	//not valid
+	boolean y = 1;
+	boolean isGood=-true;
+	int num = !5;
+	int notZero = !0;
+	}
+
+}
