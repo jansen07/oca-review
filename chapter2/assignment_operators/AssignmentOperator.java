4a1c0acb9e7a6aac94f162658ce39d5c76fac47e
Jansen <jansen.goyena@yahoo.com>
2018-09-14 16:14:06 +0800

    Assignment Operators

---
+++ chapter2/assignment_operators/AssignmentOperator.java
@@ -0,0 +1,15 @@
+public class AssignmentOperator{
+public static void main(String[] args){
+short testShort=10;
+short testMulShort = testShort*2;
+	int x = 1.0; //DOES NOT COMPILE
+short y= 1921222; //DOES NOT COMPILE
+int z= 9f; //DOES NOT COMPILE
+long l= 123456; // DOES NOT COMPILE
+float f= 1.0; // DOES NOT COMPILE
+float f1= 1; //THIS COMPILES int -> float
+//REMEMBER
+//adding .0 decimal instructs compiler to treat value as double
+}
+
+}
