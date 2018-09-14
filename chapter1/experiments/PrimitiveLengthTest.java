e33a37cd09487eb7f729ec79aefc633f7453c4c6
Jansen <jansen.goyena@yahoo.com>
2018-09-14 10:35:01 +0800

    does int have .length?
    remember that String does have .length() bacause String is an object

---
+++ chapter1/experiments/PrimitiveLengthTest.java
@@ -0,0 +1,9 @@
+public class PrimitiveLengthTest{
+
+	public static void main(String[] _test$){
+		int x=4;
+		System.out.println(x.length());
+		// the only thing to remeber is that primitive types don't have methods	
+	}
+
+}
