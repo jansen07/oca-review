54f0e68eb54b95bc0733ce143440224fc8465e60
Jansen <jansen.goyena@yahoo.com>
2018-09-14 10:57:59 +0800

    Arithmetic operators
    += is the last evaluated

---
+++ chapter2/binaryArithmeticOperators/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+	static int x;
+	public static void main(String[] args){
+		System.out.println(x);
+	}
+
+}
---
+++ chapter2/binaryArithmeticOperators/arithmetic_operations/Sample1.java
@@ -0,0 +1,20 @@
+public class Sample1{
+
+public static void main(String[] args){
+
+	int x=1;
+
+	x += 10 +( (3 * 4) * 2) - (4*5);
+	/**
+	 * (3 * 4) = 12
+	 * 10 + (12 *2) - (4*5)
+	 * 10 + 24 - 20
+	 * 34 - 20
+	 * 14
+	 * 1 + 14
+	 * 15
+	 */
+	System.out.println(x);
+}
+
+}
