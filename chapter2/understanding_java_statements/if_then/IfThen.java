4db5e769af49a4146028adb008effc16cbdb05a1
Jansen <jansen.goyena@yahoo.com>
2018-09-17 18:07:59 +0800

    if asignment happens inside if.
    if it's boolean. its valid

--- chapter2/understanding_java_statements/if_then/IfThen.java
+++ chapter2/understanding_java_statements/if_then/IfThen.java
@@ -7,6 +7,17 @@ public class IfThen{
 			System.out.println("2 is greater than 1");
 			System.out.println("I am always executed");
 
+
+
+		boolean isValid=true;
+		
+		boolean unassigned;
+
+		if(unassigned=isValid){
+			System.out.println("if returns true");
+		}
+
+			
 	}
 
 }
