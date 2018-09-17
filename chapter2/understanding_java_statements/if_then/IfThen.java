80ee681cc80d56f7a90e4d3485477b4e6d3691ac
Jansen <jansen.goyena@yahoo.com>
2018-09-17 17:12:03 +0800

    If then statement

---
+++ chapter2/understanding_java_statements/if_then/IfThen.java
@@ -0,0 +1,12 @@
+public class IfThen{
+
+	public static void main(String[] args){
+
+		// parenthesis is required
+		if(2>1)
+			System.out.println("2 is greater than 1");
+			System.out.println("I am always executed");
+
+	}
+
+}
