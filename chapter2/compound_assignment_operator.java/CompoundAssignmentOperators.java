951d52f420b382ad44957f3fd2d8a55f389bc087
Jansen <jansen.goyena@yahoo.com>
2018-09-17 14:13:24 +0800

    compound operrators += and -=
    watch out for compound statement + defining variable. ex. int x += 1; won't compile

---
+++ chapter2/compound_assignment_operator.java/CompoundAssignmentOperators.java
@@ -0,0 +1,12 @@
+public class CompoundAssignmentOperators{
+
+// only  += and -= compund operators are required in exam
+// compound operators variable should already be defined	
+	public static void main(String[] args){
+	
+	int x=1;
+		x += 1;
+		
+	}
+
+}
