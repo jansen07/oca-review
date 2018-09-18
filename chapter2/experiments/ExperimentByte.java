c729a403d7d4a6c6701dafe22cbf21cbcb4986cf
Jansen <jansen.goyena@yahoo.com>
2018-09-18 16:32:47 +0800

    bytes operations converts to int and can't assign to byte var

---
+++ chapter2/experiments/ExperimentByte.java
@@ -0,0 +1,18 @@
+public class ExperimentByte{
+
+public static void main(String[] args){
+
+
+	byte x=40,y=90;
+
+	byte z =  x + y;
+
+	System.out.println(z);
+
+	// operation makes lossy conversion because it is converted to int.
+	// though literals works in assigning value. 
+	// when operations happens it does not
+
+}
+
+}
