2c6a361eac49b6086f2296391e21a5509ad22a0f
Jansen <jansen.goyena@yahoo.com>
2018-09-18 16:38:53 +0800

    while loop and for loop requires parenthesis

---
+++ chapter2/experiments/ExperimentWhile.java
@@ -0,0 +1,19 @@
+public class ExperimentWhile{
+/*
+ * While/For loop requires ( ) parenthesis
+ * This does not compile
+ * 
+ *
+ */
+public static void main(String[] args){
+
+	int x=10;
+
+	while x > 1
+	 System.out.println(x);
+         x++;
+
+
+}
+
+}
