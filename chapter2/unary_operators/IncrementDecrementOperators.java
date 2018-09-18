271c7a45127e5cc142758cfbc39f37bd32c93aa6
Jansen <jansen.goyena@yahoo.com>
2018-09-18 09:57:31 +0800

    Unary Operator experiment, Increment Decrement operator

--- chapter2/unary_operators/IncrementDecrementOperators.java
+++ chapter2/unary_operators/IncrementDecrementOperators.java
@@ -16,6 +16,45 @@ public static void main(String[] args){
 System.out.println("x is " + x);
 System.out.println("y is " + y);
 
+	
+	int z=1;
+	int a=0;
+
+	a=z++;
+// a = 1, z =2
+	System.out.println("a: "+a+ " z:"+z);
+
+
+	a = --z;
+// a =1 z=1
+
+	System.out.println("a: "+a+ " z:"+z);
+	
+	a= ++z;
+
+	// a = 2 z=2
+	System.out.println("a: "+a+ " z:"+z);
+
+
+	x=1;
+	z=1;
+
+	x = z++ + z++;
+//	x = z= z+1  + z = z+
+	//    z 1    +  z=2
+	//    z = 3
+	//    x = 3
+	System.out.println("x: "+x+ " z:"+z);
+
+
+	x=2;
+	z=1;
+	x = z++ + z++ + x;
+	//   1     2    2
+	//   x =  5  z = 3
+	//   amazing right?
+	System.out.println("x: "+x+ " z:"+z);
 }
 
 }
+
