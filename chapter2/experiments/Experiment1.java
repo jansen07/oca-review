d8b7005ca4a262cd8457cd22e897dd3acca097e6
Jansen <jansen.goyena@yahoo.com>
2018-09-18 16:19:34 +0800

    y = x == false is valid
    y = x == false is valid

---
+++ chapter2/experiments/Experiment1.java
@@ -0,0 +1,14 @@
+public class Experiment1{
+
+
+	public static void main(String[] args){
+		boolean x=true;
+		// this compiles. true == false is false
+		boolean y = x == false;
+
+//prints false
+	System.out.println(y);	
+	
+	}
+
+}
