522eb96da88f95fb86d2cbfde415d818e06478e1
jansen07 <jansen.goyena@yahoo.com>
2018-09-20 15:50:30 +0800

    different instantiation in declaring array of int

--- chapter3/arrays/primitive/PrimitiveInt.java
+++ chapter3/arrays/primitive/PrimitiveInt.java
@@ -8,8 +8,14 @@ public class PrimitiveInt{
 		   System.out.println(number);
 		
 		}
-		int x;	// not initialized but valid	
+		int x=0;	//if not initialized and used will throw compile error	
 		System.out.println(x);// compile error line 12. invalid
+
+		//this are all valid array declaration
+		
+		int [] a = new int[2];
+		int  b[] = new int[3];
+		int c []; // new [2]int; doesn't work and same with new int[] without number
 	}
 
 
