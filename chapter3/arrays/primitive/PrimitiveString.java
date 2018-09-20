eaab6ddb07a2fddebf89771d574c6eac85521271
jansen07 <jansen.goyena@yahoo.com>
2018-09-20 14:39:31 +0800

    String array also initialized with null

---
+++ chapter3/arrays/primitive/PrimitiveString.java
@@ -0,0 +1,13 @@
+public class PrimitiveString{
+
+
+	public static void main(String[] args){
+		//just think of the new keyword.	
+		String strArray[] = new String[5]; // is initialized with null values
+		
+		for(String str: strArray)	
+			System.out.println(str);
+	}
+
+
+}
