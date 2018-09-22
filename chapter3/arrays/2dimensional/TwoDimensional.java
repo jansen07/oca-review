797a8401fd4066ab6945efeac20b7081dd77e621
jansen07 <jansen.goyena@yahoo.com>
2018-09-22 23:30:06 +0800

    2d array initialization

---
+++ chapter3/arrays/2dimensional/TwoDimensional.java
@@ -0,0 +1,16 @@
+public class TwoDimensional{
+
+	public static void main(String... args){
+	
+		int[] x[] = new int[1][1]; //2d array
+		int[] y,z [],ap[][]; //1d , 2d, 3d array , 
+
+		//int[] y, []z; this is not valid only after the variable if split
+			
+	
+	}
+
+
+
+
+}
