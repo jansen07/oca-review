d3f953787f2cddb7ff80613ea3a30b0b43e2ac2b
Jansen <jansen.goyena@yahoo.com>
2018-09-24 18:19:26 +0800

    toArray()  -> list to array. fixed size. reference points to same obj.

---
+++ chapter3/arrays/convert_array_and_list/ToArray.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.*;
+public class ToArray{
+
+	public static void main(String[] args){
+		ArrayList<Integer> nmbrList = new ArrayList<>();
+
+		nmbrList.add(4);
+		nmbrList.add(6);
+		nmbrList.add(20);	
+		Object[] obj = nmbrList.toArray();
+		Integer[] strArray=  nmbrList.toArray(new Integer[0]); // returns array with specific object, [0] zero means same size with list. or you can specify yours
+
+			//what will happen if you instantiate with length less than actual?
+		Integer  []testArray= nmbrList.toArray(new Integer[1]);	
+
+		System.out.println(Arrays.toString(strArray));
+
+		System.out.println(Arrays.toString(testArray));
+		System.out.println(Arrays.toString(obj));	
+		
+		
+		String[] x = {"4","2","3","5","1"};
+
+		List<String> list = Arrays.asList(x);
+		
+		System.out.println(list);
+		System.out.println(Arrays.toString(x));
+
+		x[0]= "100";
+
+
+		System.out.println(list); // 100,2,3,5,1
+		System.out.println(Arrays.toString(x)); // 100,2,3,5,1
+
+		list.add("test"); //runtime exception, list is fixed size
+			
+	}
+
+
+}
