574f494da7960dca0c25ef4a1d8081853f76a7ff
Jansen <jansen.goyena@yahoo.com>
2018-09-26 09:56:25 +0800

    LocalDate  method return experiment

--- chapter3/date_time/DateTimeTest.java
+++ chapter3/date_time/DateTimeTest.java
@@ -5,6 +5,14 @@ public class DateTimeTest{
 		print(LocalDate.now());
 		print(LocalTime.now());
 		print(LocalDateTime.now());
+
+		LocalDate date= LocalDate.now();
+		date.plusWeeks(1); // this returns object. so you have to reassign it to date
+		print(date);
+		date=date.plusWeeks(1);
+		print(date);
+		LocalDate date1= date.plusDays(2);
+		print(date1);
 	
 	}
 	public static void print(Object obj){
