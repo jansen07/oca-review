4075af40092f83014d80e1bdba432d1a3bde8589
Jansen <jansen.goyena@yahoo.com>
2018-09-28 13:31:43 +0800

    method tests

---
+++ chapter4/methods/method_name/MethodName.java
@@ -0,0 +1,18 @@
+public class MethodName{
+
+	/*
+	 * Method name same rules apply with variable and class naming
+	 * 
+	 */
+	public static void main(String[] args){
+		
+	
+	}
+
+
+	public void $ok(){}
+	public void _$$$(){}
+	public void 3Test(){} // compile time error. can only strat with _ $ or letters. special characters/numbers not valid
+	public void int(){} // reserved words not allowed , compile time error
+
+}
