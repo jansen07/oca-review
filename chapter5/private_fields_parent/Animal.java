3e14ba323dfb38400f8f5af6740b3dd9084328d6
jansen07 <jansen.goyena@yahoo.com>
2018-10-03 10:26:43 +0800

    Inheritance private field

---
+++ chapter5/private_fields_parent/Animal.java
@@ -0,0 +1,4 @@
+public class Animal{ // only public/default can be applied to this class, except inner classes
+	private String firstName;
+	protected String lastName;
+}
---
+++ chapter5/private_fields_parent/Lion.java
@@ -0,0 +1,10 @@
+//notice that we didn't import the Animal Class
+public class Lion extends Animal{
+
+	public void roar(){
+		System.out.println(lastName); 
+//		System.out.println(firstName); // this line will not compile because firstName is a private Field
+
+		
+	}	
+}
