d9cf8b59de240a6b06c95474fb5d170b86fc396a
Jansen <jansen.goyena@yahoo.com>
2018-10-04 18:26:06 +0800

    another abstract class compile error

--- chapter5/abstract/compile_error/Abstract.java
+++ chapter5/abstract/compile_error/Abstract.java
@@ -1,8 +1,15 @@
-public abstract class Abstract{
+public final abstract class Abstract{ // compile error
 	public abstract void printName(){} // compile error : abstract method cannot have a body
 	 
 	public abstract void test(){ // compile error :  abstract method cannot have a body
 		System.out.println("compile time error");
 	
 	}
+
+	public final abstract void finalAbstract(); //compile error abstract and final 
+
+	public final void finalConcrete(){ // this is good
+	}
+
+	private abstract void privateAbstract(); // compile error. abstract class should be implemented. that's why it can't be abstract and private
 }
