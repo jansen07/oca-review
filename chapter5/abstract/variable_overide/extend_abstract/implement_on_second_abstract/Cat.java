b14444f86296e4055ff1cb4802c3aa0fb013da15
Jansen <jansen.goyena@yahoo.com>
2018-10-05 11:32:58 +0800

    concrete class must implement all unimplemented method of abstract class
    chained abstract class with abstract methods. this methods must be implemented in the concrete class

---
+++ chapter5/abstract/variable_overide/extend_abstract/implement_on_second_abstract/Cat.java
@@ -0,0 +1,14 @@
+public abstract class Cat extends Animal{
+	
+
+	public void printName(){ // should have thesame return type
+
+		System.out.println("second abstract implementation");
+	
+	}
+
+	public abstract void printAge(); // remember!!!!! abstract method have abstract modifier!!!!!!!!
+
+
+
+}
---
+++ chapter5/abstract/variable_overide/extend_abstract/implement_on_second_abstract/Tiger.java
@@ -0,0 +1,14 @@
+public class Tiger extends Cat{
+	
+	public static void main(String[] args){
+		
+	
+	}
+
+	// implement printAge
+	public void printAge(){
+		System.out.println("printAge implementation on concrete class");
+	
+	}
+
+}
