5d38dc36c3f82f5d82492b0c55205e771605e2ee
Jansen <jansen.goyena@yahoo.com>
2018-10-05 13:54:47 +0800

    Interface assumes public abstract in method. and abstract interface

---
+++ chapter5/interface/defining_interface/without_abstract/AnimalInterface.java
@@ -0,0 +1,5 @@
+public interface AnimalInterface{ // same as -> public abstract interface AnimalInterface
+
+	void printName(); // same as public abstract . remember method must be public.
+//	protected String getName(); protected not allowed here
+}
