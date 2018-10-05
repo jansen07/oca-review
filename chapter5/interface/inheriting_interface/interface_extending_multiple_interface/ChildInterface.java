80f17698c7128bb96fa58ce5395a3410d3e34d5f
Jansen <jansen.goyena@yahoo.com>
2018-10-05 14:15:41 +0800

    Interface extending Multiple Interface with concrete class
    you should implement all the abstract method

---
+++ chapter5/interface/inheriting_interface/interface_extending_multiple_interface/ChildInterface.java
@@ -0,0 +1,4 @@
+public interface ChildInterface extends Interface1,Interface2{
+
+
+}
---
+++ chapter5/interface/inheriting_interface/interface_extending_multiple_interface/Lion.java
@@ -0,0 +1,10 @@
+public class Lion implements ChildInterface{
+	public void printInterface1(){ /// can't be lower so it will always be public if implementing interface
+	
+	}
+
+	public void printInterface2(){
+	
+	}
+
+}
