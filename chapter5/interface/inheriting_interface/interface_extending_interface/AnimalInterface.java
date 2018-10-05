0cf6020f15060a758dec5433b1ad443c90fbaf5b
Jansen <jansen.goyena@yahoo.com>
2018-10-05 14:06:10 +0800

    interface extending interface
    interface can extend interface. and can have same method signature too.

---
+++ chapter5/interface/inheriting_interface/interface_extending_interface/AnimalInterface.java
@@ -0,0 +1,3 @@
+public interface AnimalInterface{
+	void printName();
+}
---
+++ chapter5/interface/inheriting_interface/interface_extending_interface/CatInterface.java
@@ -0,0 +1,7 @@
+public interface CatInterface extends AnimalInterface{
+	void printName();
+	public static void main(String[] args){
+	
+		System.out.println("test");
+	}
+}
