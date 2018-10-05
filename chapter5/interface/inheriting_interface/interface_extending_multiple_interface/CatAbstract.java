7266d466f236fc79e6d38db2bc538c7e04138bc8
Jansen <jansen.goyena@yahoo.com>
2018-10-05 14:20:53 +0800

    abstract cant lower access of interface. always public

---
+++ chapter5/interface/inheriting_interface/interface_extending_multiple_interface/CatAbstract.java
@@ -0,0 +1,4 @@
+public abstract class CatAbstract implements ChildInterface{
+	protected void printInterface1(); // will not compile also. weaker access. abstract method can't lower access of interfaces.
+
+}
