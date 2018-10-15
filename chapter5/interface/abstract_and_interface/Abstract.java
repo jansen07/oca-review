194d9ee58cddd83fe7452b34f67662eb66090f05
Jansen <jansen.goyena@yahoo.com>
2018-10-15 18:09:47 +0800

    abstract implementing interface

---
+++ chapter5/interface/abstract_and_interface/Abstract.java
@@ -0,0 +1,1 @@
+public abstract class Abstract implements First{} // abstract class implementing interface may not implement interface abstract methods
---
+++ chapter5/interface/abstract_and_interface/First.java
@@ -0,0 +1,4 @@
+public interface First{
+
+	void printName();
+}
