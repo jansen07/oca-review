41582c5726572882aacdbd5580c59788eac2a30c
Jansen <jansen.goyena@yahoo.com>
2018-10-16 19:13:11 +0800

    Test Checked and Unchecked Exception

---
+++ chapter6/test_no_blocks/TestCatch.java
@@ -0,0 +1,21 @@
+public class TestCatch{
+
+	public static void main(String[] args){
+		
+
+		try{
+			throw new Exception();
+		
+		}catch(Exception e){
+		
+		
+		}
+		
+			throw new RuntimeException();// runtime exception
+		
+		
+	
+	}
+	
+
+}
