3839b3a7fff894267776e79a425960edbbeb3db8
Jansen <jansen.goyena@yahoo.com>
2018-10-16 19:08:46 +0800

    Checked Exception should be thrown out to main if not catched

---
+++ chapter6/test_no_blocks/Test.java
@@ -0,0 +1,11 @@
+public class Test{
+
+
+	public static void main(String[] args) throws Exception{  // exceptions must be caught up to main method
+		
+		System.out.println("Test");
+
+		throw new Exception(); // remember (); since it's declaring new object.
+	}
+
+}
